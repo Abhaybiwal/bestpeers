2b05330041c0ba35096a505d6ca9afdc5435fe1f
Abhay Biwal <61582914+Abhaybiwal@users.noreply.github.com>
2024-06-05 10:36:44 +0530

    flatten.py

--- abhay_4_june_task1/flatten.py
+++ abhay_4_june_task1/flatten.py
@@ -15,23 +15,6 @@
 #     return result
 
 nested_list = [[1, 2, [3, 4]], [5, 6], [7, (8, 9)]]
-
-def flaten(nested_list):
-    result = []
-    index = 0
-    nested_len = len(nested_list)
-
-    while index < nested_len:
-        current = nested_list[index]
-        curr_type=type(current)
-        if (curr_type is not int) and isinstance(current, curr_type):
-            nested_len += len(current) - 1
-            # nested_list[index+1:index+1] = current
-        else:
-            result.append(current)
-        index += 1
-
-    return result
         
 print(flaten(nested_list))
 
