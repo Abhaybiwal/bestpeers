74e4b82f0005afec563fea453beb3e49d196fed1
Abhay Biwal <61582914+Abhaybiwal@users.noreply.github.com>
2024-06-05 10:44:36 +0530

    Update flatten.py

--- abhay_4_june_task1/flatten.py
+++ abhay_4_june_task1/flatten.py
@@ -1,20 +1,16 @@
-# def flatten(nested):
-#     result = []
-#     for item in nested:
-#         breakpoint()
-#         try:
-#             result.extend(flatten(item))
-#         except:
-#             result.append(item)
-#     return result
+def flatten(nested):
+    result = []
+    for item in nested:
+        if isinstance(item, (list, tuple,set)):  # Check if item is a list or tuple
+            result.extend(flatten(item))
+        else:
+            result.append(item)
+    return result
 
-# nested_list = [[1, 2, [3, 4]], [5, 6], [7, [8, 9]]]
-# flattened = flatten(nested_list)
-# print(flattened)  # Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
-# print(sum(flattened))
-#     return result
+nested_list = [[1, 2, [3, 4]], [5, 6], [7, [8, 9]]]
+flattened = flatten(nested_list)
+print(flattened)  # Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 nested_list = [[1, 2, [3, 4]], [5, 6], [7, (8, 9)]]
-        
-print(flaten(nested_list))
-
+flattened = flatten(nested_list)
+print(flattened)  # Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
