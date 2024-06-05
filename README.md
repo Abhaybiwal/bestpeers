3274dfe59465ffea45ce329b8c8e3d3b6ee67f45
Neetesh NG <neeteshng15@gmail.com>
2024-06-05 10:19:15 +0530

    assignment

--- README.md
+++
@@ -1,2 +0,0 @@
-# bestpeers
-Daily update 
--- transpose.py
+++
@@ -1,3 +0,0 @@
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-transpose=[[matrix[j][i] for j in range(0,len(matrix[i]))] for i in range(0,len(matrix))]
-print(transpose)
