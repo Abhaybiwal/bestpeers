4ea4cad0e2450a94e8d05fbdc66595c6caa4db04
AbhayBiwal <abhaybiwal2@gmail.com>
2024-06-25 18:57:04 +0530

    Made some changes in the shop app and changed the database to postgresql from sqllite

--- mac/shop/models.py
+++ mac/shop/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=50)
-    category = models.CharField(max_length=50, default="")
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     subcategory = models.CharField(max_length=50, default="")
     price = models.IntegerField(default=0)
     desc = models.CharField(max_length=300)
