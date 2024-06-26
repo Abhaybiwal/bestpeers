44eb5442dc0a2e7ee403c8d4c638d5d0bc9ab35f
AbhayBiwal <abhaybiwal2@gmail.com>
2024-06-26 18:55:14 +0530

    Added the form and forms.py and form model

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,6 +6,7 @@ from . import views
 app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("contact/", views.get_name, name="contact"),
     path("<int:question_id>/", views.detail, name="detail"),
     path("<int:question_id>/results/", views.results, name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render,get_object_or_404
 
-from django.http import HttpResponse,Http404
+from django.http import HttpResponse,Http404,HttpResponseRedirect
 
 from .models import Question
 
+from .forms import NameForm
 
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return render(request,"polls/thanks.html")
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, "polls/name.html", {"form": form})
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
