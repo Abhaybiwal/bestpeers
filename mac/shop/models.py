0e749030b593d055baff1c0ff1d3a2b7e5efe918
AbhayBiwal <abhaybiwal2@gmail.com>
2024-07-02 18:50:17 +0530

    Added some javascript Cart is in progess

--- mac/shop/models.py
+++ mac/shop/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class ProductCategory(models.Model):
     name = models.CharField(max_length=50)
@@ -32,3 +32,9 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Product,Contact
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,JsonResponse
+from .models import Product,Contact,Cart
 from math import ceil
 
 def index(request):
     allProds = []
     catprods = Product.objects.values('category', 'id')
+    # data = Product.objects.filter(category = "cat")
     cats = {item['category'] for item in catprods}
     for cat in cats:
         prod = Product.objects.filter(category=cat)
         n = len(prod)
         nSlides = n // 4 + ceil((n / 4) - (n // 4))
         allProds.append([prod, range(1, nSlides), nSlides])
+
 
     # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
     # allProds = [[products, range(1, nSlides), nSlides],
@@ -33,6 +35,8 @@ def contact(request):
         desc=request.POST.get('desc', '')
         contact = Contact(name=name, email=email, phone=phone, desc=desc)
         contact.save()
+    # elif request.method=="GET":
+    #     print(request.GET.get("a"))
     return render(request, "shop/contact.html")
 
 
