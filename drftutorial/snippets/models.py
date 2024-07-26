da0cca13462c2f325add18e44faa41f68b8c5c84
AbhayBiwal <abhaybiwal2@gmail.com>
2024-07-26 19:21:08 +0530

    Added 26 July work

--- drftutorial/snippets/models.py
+++ drftutorial/snippets/models.py
@@ -5,6 +5,10 @@ from pygments.lexers import get_lexer_by_name
 from pygments.formatters.html import HtmlFormatter
 from pygments import highlight
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import pre_save, post_save,pre_delete,post_delete,m2m_changed
+
+
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
@@ -38,10 +42,21 @@ class Snippet(models.Model):
         ordering = ['created']
 
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     published_date = models.DateField()
+    authors = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return self.title
 
 
 class UserProfile(models.Model):
@@ -71,3 +86,42 @@ class Track(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+@receiver(pre_save, sender=Book)
+def pre_save_book_handler(sender, instance,*args, **kwargs):
+    print(f"About to save book: {instance.title}")
+    print(args, kwargs)
+
+
+@receiver(post_save, sender=Book)
+def post_save_book_handler(sender, instance, created,*args, **kwargs):
+    print(args,kwargs)
+    if created:
+        print(f"New book created: {instance.title}")
+    else:
+        print(f"Book updated: {instance.title}")
+
+
+# note we cant save the instance again we can assign it to some value like 
+# instanse.fild_name = instanse.another_created or upadated field in pre_save 
+# because if we try to do so we will stuck in the infinte loop 
+
+@receiver(pre_delete, sender=Book)
+def pre_delete_handler(sender, instance, **kwargs):
+    # Perform actions before the model is deleted
+    print(f"About to delete: {instance}")
+
+@receiver(post_delete, sender=Book)
+def post_delete_handler(sender, instance, **kwargs):
+    # Perform actions after the model is deleted
+    print(f"Deleted: {instance}")
+
+
+@receiver(m2m_changed,sender=Book.authors.through)
+def m2m_changed_handler(sender, instance, action,*args,**kwargs):
+    # print(args,kwargs)
+    if action == 'pre_add':
+        print("was added")
+        qs=kwargs.get("model").objects.filter(pk__in=kwargs.get("pk_set"))
+        print(qs.count())
